efbbbdacea2b093f428065efdc2c30f0465022ff
Martin Ondráček <34232362+Thromulus@users.noreply.github.com>
2023-04-01 21:10:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 VERSION=1
 docker build -t fivpuupoldqschnjkm/phr_app:v$VERSION .
-docker push fivpuupoldqschnjkm/phr_app:v1
+docker push fivpuupoldqschnjkm/phr_app:v$VERSION
 sleep 20
 kubectl apply -f manifest
